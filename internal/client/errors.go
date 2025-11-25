0c9773f902d52f8c2610d93ba0b9ba98a6cd2a9e
Bruno Espino <brunoespi7@gmail.com>
2025-11-25 14:13:20 -0300

    Improve project structure

--- internal/client/errors.go
+++ internal/client/errors.go
@@ -1,14 +1,15 @@
 // Copyright (c) Pydantic, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-package provider
+package client
 
 import (
 	"errors"
 	"net/http"
 )
 
-func isRateLimitError(err error) bool {
+// IsRateLimitError reports whether the error represents an HTTP 429.
+func IsRateLimitError(err error) bool {
 	var apiErr *APIError
 	if errors.As(err, &apiErr) {
 		return apiErr.StatusCode == http.StatusTooManyRequests
---
+++ internal/provider/write_token_resource_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) Pydantic, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccWriteTokenResource(t *testing.T) {
+	t.Parallel()
+
+	projectName := fmt.Sprintf("acc-write-token-%s", acctest.RandStringFromCharSet(6, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWriteTokenResourceConfig(projectName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("logfire_write_token.test", tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("logfire_write_token.test", tfjsonpath.New("project_id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("logfire_write_token.test", tfjsonpath.New("description"), knownvalue.StringExact(writeTokenDescriptionValue)),
+					statecheck.ExpectKnownValue("logfire_write_token.test", tfjsonpath.New("token_prefix"), knownvalue.NotNull()),
+				},
+			},
+			{
+				Config: testAccWriteTokenResourceConfig(projectName),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectEmptyPlan(),
+					},
+				},
+			},
+		},
+	})
+}
+
+func testAccWriteTokenResourceConfig(projectName string) string {
+	return fmt.Sprintf(`%s
+
+resource "logfire_project" "test" {
+  name        = %q
+  description = "Acceptance test project for write token"
+}
+
+resource "logfire_write_token" "test" {
+  project_id = logfire_project.test.id
+}
+`, testAccProviderConfig(), projectName)
+}
